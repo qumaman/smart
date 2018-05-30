4630a61818946c8465948dd561d8ca1e21979fc1
amanzhol <qum.aman@gmail.com>
2018-05-30 13:35:27 +0600

    First upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var journalRouter = require('./routes/journal');
 
-var app = express();
+var app = express(); 
  
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
